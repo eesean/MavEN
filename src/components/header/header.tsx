175104810ceb0ea70c324981539a712bbe6726c0
HoWeiHao <howeihao3105@gmail.com>
2024-02-07 03:08:06 +0800

    Polished volunteer dashboard

---
+++ src/components/header/header.tsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames';
+import styles from './header.module.scss';
+
+export interface HeaderProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const Header = ({ className }: HeaderProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <div className={styles.logo}>
+                <img src="../icons/favicon.png" alt="Logo image" className={styles['logo-image']} />
+                <h1 className={styles['logo-text']}>MAvEN</h1>
+            </div>
+            <img src="../icons/profile.png" alt="profile image" className={styles.avatar} />
+        </div>
+    );
+};
---
+++ src/components/navigation-bar/navigation-bar.tsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames';
+import styles from './navigation-bar.module.scss';
+
+export interface NavigationBarProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const NavigationBar = ({ className }: NavigationBarProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <div />
+            <div className={styles['nav-button']}>
+                <img src="../icons/house.png" alt="Home icon" className={styles['nav-icons']} />
+                <p className={styles['nav-text']}>Home</p>
+            </div>
+            <div className={styles['nav-button']}>
+                <img src="../icons/activities.png" alt="" className={styles['nav-icons']} />
+                <p className={styles['nav-text']}>Activities</p>
+            </div>
+            <div className={styles['nav-button']}>
+                <img src="../icons/certificate.png" alt="" className={styles['nav-icons']} />
+                <p className={styles['nav-text']}>Certificate</p>
+            </div>
+        </div>
+    );
+};

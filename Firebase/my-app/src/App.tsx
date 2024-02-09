3af8f313fa4941127d6e18c2f96e3d4dd0e1a830
Tan Ee Sean <110977266+eesean@users.noreply.github.com>
2024-02-09 16:29:01 +0800

    fix routes (start at login page after npm run start)

--- Firebase/my-app/src/App.tsx
+++ Firebase/my-app/src/App.tsx
@@ -1,27 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useAuth } from './contexts/AuthContext'; // Assuming this hook provides user and auth status
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { useAuth } from './contexts/AuthContext'; // Assuming this hook provides user and auth status
 import { AuthProvider } from './contexts/AuthContext';
 import { Homepage } from './homepage/homepage';
-import AdminPage from './pages/AdminPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
-  const { currentUser, userRole } = useAuth(); // Adjust according to your actual auth context
+  // const { currentUser, userRole } = useAuth(); // Adjust according to your actual auth context
 
   return (
     <Router>
       <AuthProvider>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route
-            path="/admin"
-            element={
-              currentUser && userRole === 'administrator' ? <AdminPage /> : <Navigate to="/login" replace />
-            }
-          />
           <Route
             path="/homepage"
             element={
